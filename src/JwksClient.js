07bbcdf838fc1843af6bfb25a1a96dc29aff56e1
Hérikz Nawarro <herikz.nawarro@gmail.com>
2020-01-19 09:08:11 -0300

    Add methods to get converted keys from pre-downloaded content

--- src/JwksClient.js
+++ src/JwksClient.js
@@ -33,6 +33,72 @@ export class JwksClient {
     }
   }
 
+  _convertKeys = (keys, cb) => {
+    const signingKeys = keys.filter((key) => {
+      if(key.kty !== 'RSA'){
+        return false;
+      }
+      if(!key.kid){
+        return false;
+      }
+      if(key.hasOwnProperty('use') && key.use !== 'sig'){
+        return false;
+      }
+      return ((key.x5c && key.x5c.length) || (key.n && key.e));
+    })
+    .map(key => {
+      if (key.x5c && key.x5c.length) {
+        return {
+          kid: key.kid,
+          nbf: key.nbf,
+          publicKey: certToPEM(key.x5c[0])
+        };
+      } else {
+        return {
+          kid: key.kid,
+          nbf: key.nbf,
+          rsaPublicKey: rsaPublicKeyToPEM(key.n, key.e)
+        };
+      }
+    })
+
+    if (!signingKeys.length) {
+      return cb(new JwksError('The JWKS endpoint did not contain any signing keys'));
+    }
+
+    this.logger('Signing Keys:', signingKeys);
+
+    cb(null, signingKeys)
+  }
+
+  extractSigningKey({ keys }, kid, cb) {
+    return this._convertKeys(keys, (err, signingKeys) => {
+      if (err) {
+        return cb(err)
+      }
+
+      if (!kid) {
+        return cb(new ArgumentError('The kid must not be empty'))
+      }
+
+      this.logger(`Extracting signing key for '${kid}'`);
+
+      return cb(null, signingKeys.find(k => k.kid === kid))
+    })
+  }
+
+  extractSigningKeys({ keys }, cb) {
+    return this._convertKeys(keys, (err, signingKeys) => {
+      if (err) {
+        return cb(err)
+      }
+
+      this.logger(`Extracting all signing keys`);
+
+      return cb(null, signingKeys)
+    })
+  }
+
   getKeys(cb) {
     this.logger(`Fetching keys from '${this.options.jwksUri}'`);
     request({
@@ -56,50 +122,22 @@ export class JwksClient {
   }
 
   getSigningKeys(cb) {
-    this.getKeys((err, keys) => {
-      if (err) {
-        return cb(err);
+    this.getKeys((err0, keys) => {
+      if (err0) {
+        return cb(err0);
       }
 
       if (!keys || !keys.length) {
         return cb(new JwksError('The JWKS endpoint did not contain any keys'));
       }
 
-      const signingKeys = keys
-          .filter((key) => {
-            if(key.kty !== 'RSA'){
-              return false;
-            }
-            if(!key.kid){
-              return false;
-            }
-            if(key.hasOwnProperty('use') && key.use !== 'sig'){
-              return false;
-            }  
-            return ((key.x5c && key.x5c.length) || (key.n && key.e));
-        })
-        .map(key => {
-          if (key.x5c && key.x5c.length) {
-            return {
-              kid: key.kid,
-              nbf: key.nbf,
-              publicKey: certToPEM(key.x5c[0])
-            };
-          } else {
-            return {
-              kid: key.kid,
-              nbf: key.nbf,
-              rsaPublicKey: rsaPublicKeyToPEM(key.n, key.e)
-            };
-          }
-        });
+      this._convertKeys(keys, (err1, signingKeys) => {
+        if (err1) {
+          return cb(err1);
+        }
 
-      if (!signingKeys.length) {
-        return cb(new JwksError('The JWKS endpoint did not contain any signing keys'));
-      }
-
-      this.logger('Signing Keys:', signingKeys);
-      return cb(null, signingKeys);
+        return cb(null, signingKeys);
+      })
     });
   }
 
@@ -112,6 +150,7 @@ export class JwksClient {
       }
 
       const key = keys.find(k => k.kid === kid);
+
       if (key) {
         return cb(null, key);
       } else {
